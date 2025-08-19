d3525e9658a3d8d00ae534af0b196c3fee26f704
Radhey Radhey <bibekmondal906@gmail.com>
2025-08-19 16:04:23 +0530

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,9 +1,8 @@
-// Import Firebase SDK
+// assets/js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// ✅ Your Firebase Config (जो तुमने दिया है)
 const firebaseConfig = {
   apiKey: "AIzaSyAGXN6dWthp8Z4aYP7JvW38O32aG8KRGR4",
   authDomain: "ticket-web-9fa9e.firebaseapp.com",
@@ -13,9 +12,6 @@ const firebaseConfig = {
   appId: "1:801985358951:web:29e558c044ecaaa5c8f8c2"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
